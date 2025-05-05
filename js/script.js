12499bf26da59d36ec97b4d521447ee6f1451934
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-05-05 13:05:02 +0200

    feat: init project

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log("ciao")
+
+// Snack 1
+
+let bici1 = {
+    nome: "Mountain Bike",
+    peso: 15
+};
+  
+let bici2 = {
+    nome: "Bici da Corsa",
+    peso: 10
+};
+  
+let bici3 = {
+    nome: "Bici da Città",
+    peso: 12
+};
+  
+let bici4 = {
+    nome: "E-Bike",
+    peso: 20
+};
